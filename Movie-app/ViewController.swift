7acb6b7f45a27516ce990621f3c760a9827af4c0
asif-emumba <asif.hussain@emumba.com>
2025-01-17 23:10:56 +0500

    Initial Commit

---
+++ Movie-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-app
+//
+//  Created by Emumba on 17/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

8ec397fbe823abf8a296e9dcade36b1b66ba0573
asif-emumba <asif.hussain@emumba.com>
2025-01-18 01:13:14 +0500

    initial commit

---
+++ Movie-app/Utils/ColorsHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ColorsHelper.swift
+//  assesment-movies-app
+//
+//  Created by Emumba on 17/01/2025.
+//
+
+import UIKit
+
+enum CustomColors{
+    static let backgroundColor = UIColor(named: "backgroundColor")
+    static let textColor = UIColor(named: "textColor")
+}

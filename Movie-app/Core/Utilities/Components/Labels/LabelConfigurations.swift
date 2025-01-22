b62d11fc0d5aabbd6e98514538764f4e9ad20502
asif-emumba <asif.hussain@emumba.com>
2025-01-22 11:01:23 +0500

    coming soon movies added

--- Movie-app/Core/Utilities/Components/Labels/LabelConfigurations.swift
+++ Movie-app/Core/Utilities/Components/Labels/LabelConfigurations.swift
@@ -117,3 +117,94 @@ struct PriceLabelConfiguration: LabelConfigurable {
     var font: UIFont { .systemFont(ofSize: 24, weight: .bold) }
 }
 
+struct UpComingMovieNameLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .bold) }
+    var textColor: UIColor { CustomColors.yellowColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct UpComingMovieLabelConfigurations: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 12, weight: .regular) }
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct ServicesLabelConfigurations: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .medium) }
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .center }
+}
+
+struct MovieDescriptionLabelConfigurations: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .medium) }
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct MovieDetailsLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .bold) }
+    var textColor: UIColor { CustomColors.textColor  ?? .white}
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct MovieDetailsInfoLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    var textColor: UIColor { CustomColors.subHeadingColor  ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct MovieDetailsNameLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 24, weight: .bold) }
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct MovieDetailsRow: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    var textColor: UIColor { CustomColors.grayShadeColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct MovieStoryLineLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    var textColor: UIColor { .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct MovieDirectorNameLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 14, weight: .regular) }
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct CinemaNameLabelConfiguration: LabelConfigurable {
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+    var font: UIFont { .systemFont(ofSize: 20, weight: .bold) }
+}
+
+struct AppBarLabelConfiguration: LabelConfigurable {
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+    var font: UIFont { .systemFont(ofSize: 28, weight: .bold) }
+}
+
+struct PriceLabelConfiguration: LabelConfigurable {
+    var textColor: UIColor {
+        CustomColors.yellowColor?.withAlphaComponent(0.8) ?? .yellow.withAlphaComponent(0.8)
+    }
+    var textAlignment: NSTextAlignment { .left }
+    var font: UIFont { .systemFont(ofSize: 24, weight: .bold) }
+}
+
+struct UpComingMovieNameLabelConfiguration: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 16, weight: .bold) }
+    var textColor: UIColor { CustomColors.yellowColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}
+
+struct UpComingMovieLabelConfigurations: LabelConfigurable {
+    var font: UIFont { .systemFont(ofSize: 12, weight: .regular) }
+    var textColor: UIColor { CustomColors.textColor ?? .white }
+    var textAlignment: NSTextAlignment { .left }
+}

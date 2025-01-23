61f9219a3e15be2114ab688e41717becb2056e50
asif-emumba <asif.hussain@emumba.com>
2025-01-23 12:01:56 +0500

    correct name for loading collection view cell

--- Movie-app/Classes/UICollectionViewSections/CollectionViewSectionItem/LoadingSectionCellItem.swift
+++ Movie-app/Classes/UICollectionViewSections/CollectionViewSectionItem/LoadingSectionCellItem.swift
@@ -11,7 +11,7 @@ import UIKit
 final class LoadingSectionCellItem: CollectionViewCellItem {
     
     func cellForItem(at indexPath: IndexPath, in collectionView: UICollectionView) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LoadingIndicatorCell.identifier, for: indexPath) as! LoadingIndicatorCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LoadingIndicatorCollectionViewCell.identifier, for: indexPath) as! LoadingIndicatorCollectionViewCell
         cell.activityIndicator.startAnimating()
         return cell
     }

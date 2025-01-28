af173e0f7e3c7be14d46f4a11d9cb018aa24c5c3
asif-emumba <asif.hussain@emumba.com>
2025-01-28 11:23:18 +0500

    modification added

--- Movie-app/Modules/MovieDetails/Model/Cinema.swift
+++ Movie-app/Modules/MovieDetails/Model/Cinema.swift
@@ -13,15 +13,16 @@ struct Cinema: Codable {
     let address: String
     let city: String
     let logo: String
+    let price: String
     var isSelected: Bool
 }
 
 extension Cinema {
     static var mockData: [Cinema] {
         return [
-            Cinema(id: 1, name: "Vincom Ocean Park CGV", address: "4.55 km | Da Ton, Gia Lam", city: "Ha Noi", logo: Assets.Cinema.icCinema, isSelected: false),
-            Cinema(id: 2, name: "Aeon Mall CGV", address: "9.32 km | 27 Co Linh, Long Bien", city: "Ha Noi", logo: Assets.Cinema.icCinema2, isSelected: false),
-            Cinema(id: 3, name: "Lotte Cinema Long Bien", address: "14.3 km | 7-9 Nguyen Van Linh, Long Bien", city: "Ha Noi", logo: Assets.Cinema.icCinema, isSelected: false)
+            Cinema(id: 1, name: "Vincom Ocean Park CGV", address: "4.55 km | Da Ton, Gia Lam", city: "Ha Noi", logo: Assets.Cinema.icCinema, price: "210.000 VND", isSelected: false),
+            Cinema(id: 2, name: "Aeon Mall CGV", address: "9.32 km | 27 Co Linh, Long Bien", city: "Ha Noi", logo: Assets.Cinema.icCinema2,price: "260.000 VND", isSelected: false),
+            Cinema(id: 3, name: "Lotte Cinema Long Bien", address: "14.3 km | 7-9 Nguyen Van Linh, Long Bien", city: "Ha Noi", logo: Assets.Cinema.icCinema,price: "290.000 VND", isSelected: false)
         ]
     }
 }

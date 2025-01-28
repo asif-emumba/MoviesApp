7c4de3d4905c8f770d0b49002cb3f333c42213f5
asif-emumba <asif.hussain@emumba.com>
2025-01-29 01:09:05 +0500

    price+ seat info section

---
+++ Movie-app/Modules/Payment/Model/SeatsInfo.swift
@@ -0,0 +1,13 @@
+//
+//  SeatsInfo.swift
+//  Movie-app
+//
+//  Created by Asif-emumba on 28/01/2025.
+//
+
+import Foundation
+
+struct MovieOrderInfo {
+    var selectedSeats: [Seat]
+    var totalPrice: Cinema
+}

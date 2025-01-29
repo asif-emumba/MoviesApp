a319b2e0bcb3a25d392ef5c11dcd92a22b0f84c3
asif-emumba <asif.hussain@emumba.com>
2025-01-29 12:38:43 +0500

    add renamed name

--- Movie-app/Modules/Home/View/CollectionViewCells/LoadingCell.swift
+++ Movie-app/Modules/Home/View/CollectionViewCells/LoadingCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class LoadingCellItem: CollectionViewCellItem {
     func cellForItem(at indexPath: IndexPath, in collectionView: UICollectionView) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LoadingIndicatorCell.identifier, for: indexPath) as! LoadingIndicatorCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LoadingIndicatorCollectionViewCell.identifier, for: indexPath) as! LoadingIndicatorCollectionViewCell
         cell.activityIndicator.startAnimating()
         return cell
     }
